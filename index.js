81673957fff0075bd1dc00854ec75c3edb6cf476
Ian Kimani <ianmwng@gmail.com>
2023-09-25 20:11:38 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records){
+    const winningRecord = records.find(records => records.result === "W")
+     if(winningRecord){
+        return winningRecord.year;
+     } else {
+        return undefined;
+     }
+}
